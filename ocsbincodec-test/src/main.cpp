51d14e72ffce3fa44ae7fa52a5f3a32843902224
Fabio Jun Takada Chino <fchino@opencs.com.br>
2015-03-19 06:01:16 -0300

    Initial version of the unit test program.

---
+++ ocsbincodec-test/src/main.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+	int retval;
+
+	std::cout << "Running main() from gtest_main.cc\n";
+	testing::InitGoogleTest(&argc, argv);
+	retval = RUN_ALL_TESTS();
+
+	return retval;
+}
+
